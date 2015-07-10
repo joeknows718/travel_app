13c04fab8f9b5b93c318693564d9ece13b211228
JoeC718 <joe.carrano@colorofchange.org>
2015-07-10 14:13:48 -0400

    put fortune app in its own module and required it.

--- travel_app.js
+++ travel_app.js
@@ -7,19 +7,22 @@ var app = express()
 var handlebars = require('express-handlebars')
 	.create({defaultLayout:'main'});
 
+var fortune = require('./lib/fortune.js')
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars')
 
 app.set('port', process.env.PORT || 3000)
 
-app.use(express.static(__dirname + '/public')); 
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(req, res){
 	res.render('home');
 });
 
 app.get('/about', function(req, res){
-	res.render('about');
+	res.render('about', {fortune: fortune.getFortune});
 });
 //custom 404 page 
 
